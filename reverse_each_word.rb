6096070fcb081fbf11cca869ebb37cdc326eeb7a
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-01 18:32:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence1)
   split_string = sentence1.split('')
- split_string.each_with_object([]){|e, a| a.unshift(e)}.join
+ reverse_string = split_string.each_with_object([]){|e, a| a.unshift(e)}.join
+ reverse_string.reverse!
 end
  

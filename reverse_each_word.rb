4754432b723bdf06a86d31babf00184cc27318e4
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-01 19:09:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
-  split_string = sentence1.split('')
- reverse_string = split_string.each_with_object([]){|e, a| a.unshift(e).reverse!}.join
+  #split_string = sentence1.split('')
+ #reverse_string = split_string.each_with_object([]){|e, a| a .unshift(e)}.join
  
 end
  
